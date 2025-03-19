2fd2eec49e25c25fad56345f3bd21f32f6bb0ebf
Suhanabc <bcsuhana266@gmail.com>
2025-03-19 15:47:19 +0530

    first

--- server.js
+++ server.js
@@ -5,7 +5,17 @@ const router = require('./routes/authRoutes');
 const cartrouter = require('./routes/cartRoutes');
 const app = express();
 
-app.use(cors())
+const allowedOrigins = ["ecom-fronted-9zcjiw9h4-suhanas-projects-3ffb18f1.vercel.app", "ecom - fronted - taupe.vercel.app"]
+app.use(cors({
+    origin: function(origin, callback) {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error("Not allowed by CORS"));
+        }
+    },
+    credentials: true, // Allows cookies and authentication headers if needed
+}))
 app.use(express.json())
 
 connectDB()
