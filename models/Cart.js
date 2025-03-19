76187a32210b136bb6feb65c4dd2705577fb9fb9
Suhanabc <bcsuhana266@gmail.com>
2025-03-19 10:12:11 +0530

    first

---
+++ models/Cart.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+const cartSchema = mongoose.Schema({
+    userId: String,
+    items: [{
+        productId: Number,
+        quantity: Number,
+        name: String,
+        price: Number
+    }]
+
+})
+module.exports = mongoose.model('Cart', cartSchema)
